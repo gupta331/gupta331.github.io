4efdaad8325608e9d6da76aa07838bf31d72439a
Aanya Gupta <aanyagupta@Aanyas-MacBook-Pro.local>
2017-11-07 10:54:46 -0500

    adding js-4

--- aw-track-5/aw-track-5-5/assets/js/app.js
+++ aw-track-5/aw-track-5-5/assets/js/app.js
@@ -1,7 +1,19 @@
-$('.button').click(function(){
-    if($(this).hasClass('active')){
-        $(this).removeClass('active')
-    } else {
-        $(this).addClass('active')
-    }
+$('.button-1').click(function() {
+	$(this).addClass('hide');
 });
+
+$('.button-2').click(function() {
+	$(this).addClass('hide');
+
+});
+
+$('.button-3').click(function() {
+	$(this).addClass('hide');
+
+});
+
+$('.button-4').click(function() {
+	$(this).addClass('hide');
+
+});
+
